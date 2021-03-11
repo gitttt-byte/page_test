3292017664d5a3351d6c330ba0e202a856fb445e
gitttt-byte <80458735+gitttt-byte@users.noreply.github.com>
2021-03-11 21:33:08 +0900

    Add files via upload

---
+++ chobee-test/js/data.js
@@ -0,0 +1,131 @@
+const qnaList = [
+  {
+    q: '1. 쵸비가 가장 좋아하는 장난감은?',
+    a: [
+      { answer: 'a. 보잉보잉', score: 0 },
+      { answer: 'b. 공놀이', score: 0 },
+      { answer: 'c. 문어칭구칭구', score: 1 }
+    ]
+  },
+  {
+    q: '2. 쵸비의 몸무게는?',
+    a: [
+      { answer: 'a. 3kg대', score: 0},
+      { answer: 'b. 4kg대', score: 0 },
+      { answer: 'c. 5kg대', score: 0 },
+      { answer: 'd. 6kg대', score: 1 }
+    ]
+  },
+  {
+    q: '3. 쵸비의 나이는?',
+    a: [
+      { answer: 'a. 1살', score: 0 },
+      { answer: 'b. 2살', score: 0 },
+      { answer: 'c. 3살', score: 0 },
+      { answer: 'd. 4살', score: 1 }
+    ]
+  },
+  {
+    q: '4. 쵸비가 힘들때 하는 자세는?',
+    a: [
+      { answer: 'a. 다리 쭉피고 엎드리기', score: 1 },
+      { answer: 'b. 식빵자세', score: 0 },
+      { answer: 'c. 발라당', score: 0 },
+      { answer: 'd. 원기둥자세', score: 0 }
+    ]
+  },
+  {
+    q: '5. 쵸비의 울음소리는?',
+    a: [
+      { answer: 'a. 야옹', score: 0 },
+      { answer: 'b. 삐약', score: 1 },
+      { answer: 'c. 애옹', score: 0 },
+      { answer: 'd. 먕먕', score: 0 }
+    ]
+  },
+  {
+    q: '6. 쵸비보다 몸무게가 더 나가는 고양이는?',
+    a: [
+      { answer: 'a. 크리미', score: 0 },
+      { answer: 'b. 체다', score: 1 },
+      { answer: 'c. 꼬부기', score: 0 }
+    ]
+  },
+  {
+    q: '7. 쵸비가 가장 못하는 것은?',
+    a: [
+      { answer: 'a. 화장실 뒷정리', score: 1 },
+      { answer: 'b. 음식 다먹기', score: 0 },
+      { answer: 'c. 공굴리기', score: 0 },
+      { answer: 'd. 솜방망이 뇸뇸', score: 0 }
+    ]
+  },
+  {
+    q: '8. 쵸비가 문어칭구칭구를 물고 주로 꾹꾹이 하는 장소는?',
+    a: [
+      { answer: 'a. 침대', score: 0 },
+      { answer: 'b. 담요', score: 0 },
+      { answer: 'c. 집사', score: 0 },
+      { answer: 'd. 쵸비 인형', score: 0 },
+      { answer: 'e. 꼬부기 인형', score: 1 }
+    ]
+  },
+  {
+    q: '9. 쵸비의 mbti는?',
+    a: [
+      { answer: 'a. ENTJ', score: 0 },
+      { answer: 'b. ENFJ', score: 0 },
+      { answer: 'c. ENFP', score: 1 },
+      { answer: 'd. ENTP, score: 0 }
+    ]
+  },
+  {
+    q: '10. 다음 특징 중 쵸비의 특징이 아닌 것은?',
+    a: [
+      { answer: 'a. 귀엽다', score: 0 },
+      { answer: 'b. 요정같다', score: 0 },
+      { answer: 'c. 엉뚱하다', score: 0 },
+      { answer: 'd. 외부인을 경계한다', score: 0 },
+      { answer: 'e. 다리가 길다', score: 1 },
+      { answer: 'f. 쵸또먹', score: 0 }
+    ]
+  }
+]
+
+const infoList = [
+  {
+    from: 0,
+    to: 3,
+    mLeft: '10%',
+    name: '쵸비 유튜브 구독 하세요',
+    desc: '쵸비 퀴즈를 거의 틀리시다니! 얼른 귀여운 쵸비 유튜브 구독하세요'
+  },
+  {
+    from: 4,
+    to: 5,
+    mLeft: '25%',
+    name: '쵸비 입덕 시작',
+    desc: ''
+  },
+ {
+    from: 5,
+    to: 7,
+    mLeft: '55%',
+    name: '쵸비 구독자',
+    desc: ''
+  },
+  {
+    from: 8,
+    to: 9,
+    mLeft: '70%',
+    name: '쵸비 덕후',
+    desc: ''
+  },
+  {
+    from: 10,
+    to: 10,
+    mLeft: '100%',
+    name: '쵸비 친구',
+    desc: ''
+  }
+]
